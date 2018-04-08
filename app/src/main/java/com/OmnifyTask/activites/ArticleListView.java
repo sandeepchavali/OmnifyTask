d7de1333169d3939208f36904573d7b378ee1234
c.chavali.sandeep@gmail.com <c.chavali.sandeep@gmail.com>
2018-04-08 20:07:36 +0530

    Added ArticlesListView

--- app/src/main/java/com/OmnifyTask/activites/ArticleListView.java
+++ app/src/main/java/com/OmnifyTask/activites/ArticleListView.java
@@ -7,6 +7,9 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -14,6 +17,7 @@ import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import com.OmnifyTask.R;
+import com.OmnifyTask.adapter.StoryListAdapter;
 import com.OmnifyTask.databaseTable.StoryDetails;
 import com.OmnifyTask.databaseTable.TopStoryList;
 import com.OmnifyTask.gsonModel.StoryItems;
@@ -34,6 +38,7 @@ public class ArticleListView extends AppCompatActivity {
 
     private static final String UNIQUE_ID = "UNIQUE_ID";
     int start_count = 0, limit_count = 10;
+    RecyclerView article_Recycler;
     private Dialog dialog;
     private ProgressBar mProgressView;
     private Realm myRealm;
@@ -56,22 +61,23 @@ public class ArticleListView extends AppCompatActivity {
 
         myRealm = Realm.getDefaultInstance();
 
+        article_Recycler = findViewById(R.id.article_Recycler);
+
+
         final RealmResults<TopStoryList> results = myRealm.where(TopStoryList.class).findAll();
         RealmResults<StoryDetails> storyResult = myRealm.where(StoryDetails.class).findAll();
 
 
         if (storyResult.size() != 0) {
+            // get the size of the list in the story
             start_count = storyResult.size();
             limit_count = start_count + 10;
-            Log.e("start_count on create", storyResult.size() + "");
-
         } else {
             start_count = 0;
         }
 
         if (results.size() == 0) {
 //            clearDB();
-
             //call the Api to load the TOP storeis Api
             Topstoies();
         } else {
@@ -124,6 +130,7 @@ public class ArticleListView extends AppCompatActivity {
 
     public void ArticleDetials(String id) {
 
+//        showProgressBar();
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
         Call<StoryItems> atricledetails = apiService.articledetailsData(id);
@@ -132,6 +139,7 @@ public class ArticleListView extends AppCompatActivity {
             @Override
             public void onResponse(Call<StoryItems> call, Response<StoryItems> response) {
 
+//                hideProgress();
                 if (response.isSuccessful()) {
                     myRealm.beginTransaction();
 
@@ -189,6 +197,7 @@ public class ArticleListView extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<StoryItems> call, Throwable t) {
+//                hideProgress();
 
             }
         });
@@ -209,15 +218,26 @@ public class ArticleListView extends AppCompatActivity {
 
         RealmResults<TopStoryList> results = myRealm.where(TopStoryList.class).findAll();
 
-
+        showProgressBar();
         for (int i = start_count; i < limit_count; i++) {
 
-            Log.e("i", i + "");
             ArticleDetials(results.get(i).getTopstoryid() + "");
 
         }
 
+        hideProgress();
+
         RealmResults<StoryDetails> storyResult = myRealm.where(StoryDetails.class).findAll();
+        Log.e("storyResult", String.valueOf(storyResult.size()));
+
+        RealmList<StoryDetails> list = new RealmList<>();
+        list.addAll(storyResult);
+
+        StoryListAdapter madpter = new StoryListAdapter(ArticleListView.this, list);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        article_Recycler.setLayoutManager(mLayoutManager);
+        article_Recycler.setItemAnimator(new DefaultItemAnimator());
+        article_Recycler.setAdapter(madpter);
 
 
     }
---
+++ app/src/main/java/com/OmnifyTask/adapter/StoryListAdapter.java
@@ -0,0 +1,83 @@
+package com.OmnifyTask.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.OmnifyTask.R;
+import com.OmnifyTask.databaseTable.StoryDetails;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import io.realm.RealmList;
+
+/**
+ * Created by chavali on 2018-04-08.
+ */
+
+public class StoryListAdapter extends RecyclerView.Adapter<StoryListAdapter.ViewHolder> {
+
+    Context context;
+    RealmList<StoryDetails> mdata;
+
+
+    public StoryListAdapter(Context context, RealmList<StoryDetails> mdata) {
+        this.context = context;
+        this.mdata = mdata;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.story_list_item, parent, false);
+        context = parent.getContext();
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.votes.setText(mdata.get(position).getScore() + "");
+        holder.title.setText(mdata.get(position).getTitle());
+        holder.url.setText(mdata.get(position).getUrl());
+        holder.author.setText("." + mdata.get(position).getBy());
+        holder.comments.setText(mdata.get(position).getComments().size() + "");
+
+
+        // convert seconds to milliseconds
+        Date date = new java.util.Date(mdata.get(position).getTime() * 1000L);
+        // the format of your date
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // give a timezone reference for formatting (see comment at the bottom)
+        //        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+5:30"));
+        String formattedDate = sdf.format(date);
+        System.out.println(formattedDate);
+        holder.time.setText(formattedDate + "");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mdata.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView votes, title, url, time, author, comments;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            votes = itemView.findViewById(R.id.votes);
+            time = itemView.findViewById(R.id.time);
+            title = itemView.findViewById(R.id.title);
+            url = itemView.findViewById(R.id.url);
+            author = itemView.findViewById(R.id.author);
+            comments = itemView.findViewById(R.id.comments);
+        }
+    }
+
+}
